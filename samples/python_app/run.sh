7283db7dea37f7188813f713c99bdb8a281533ea
Szymon Józefowicz <szymon@statoilfuelretail.com>
2022-05-27 12:35:50 +0200

    Update run.sh

--- samples/python_app/run.sh
+++ samples/python_app/run.sh
@@ -2,6 +2,7 @@ sudo apt update
 sudo apt install python3-pip
 python3 -m pip install opencensus-ext-azure
 python3 -m pip install opencensus-ext-flask
+sudo apt install python3-flask
 export FLASK_APP=application.py
 flask run -h 0.0.0.0
   
